12688776b85356095159eec94e1e0b7755ac3be8
sapna <sapnapastapure@gmail.com>
2017-11-20 18:16:47 +0530

    changes for component

--- app.js
+++ app.js
@@ -4,7 +4,33 @@
 	document.getElementById('content')
 	)
 */
-	var h1=React.createElement('a',{href:'demo',target:'_blank',title:'Google Link'},'Koimoi world');
-	var content=document.getElementById('content');
-ReactDOM.render(h1,content);
 
+
+/**************** hello world ***************/
+/*	var h1=React.createElement('a',{href:'demo',target:'_blank',title:'Google Link'},'Koimoi world');
+	var content=document.getElementById('content');
+ReactDOM.render(h1,content);*/
+
+/**************** multiple componenet  ***************/
+
+		/*var h1=React.createElement('a', null,'Koimoi world');
+		var p=React.createElement('p', null,'A Quick jump to React ');
+		var content=document.getElementById('content');
+		ReactDOM.render(React.createElement('div',null,h1,p),content);
+
+		*/
+	
+/************************************************************************************************************************************************
+
+****************************************************  step 2 ********************************************************************************/
+class CustomerComponenet extends React.Componenet{
+	render(){
+		return React.createElement('h1',null,'First Component');
+	}
+}
+
+var firstConponent=React.createElement(CustomerComponenet,null);
+
+var content=document.getElementById('content');
+
+ReactDOM.render(firstConponent,componenet)
