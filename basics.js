f9a159cbbd8bada6622678b5e278caf891cbaefc
sapna <sapnapastapure@gmail.com>
2017-11-23 00:39:00 +0530

    basic file added

---
+++ basics.js
@@ -0,0 +1,25 @@
+
+
+
+
+closing tag is manadatory in jsx  that can be self close not like Html img or p tag 
+
+
+
+State vs props
+
+State :used for updating within component
+State are mutable changebale 
+
+define this.state
+
+update this.setState
+
+Props :Used to update parrent to child
+
+
+Two types of component
+
+	Statfull
+
+	Stateless
